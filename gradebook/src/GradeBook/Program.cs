9e6aecb5293fdfc36cd64e76ec97fdfd1777cdcc
Alex Wong de Freitas <otaru1921@gmail.com>
2021-04-06 17:29:48 -0300

    WIP: Chapter 3 - C# Syntax

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,6 +6,10 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var x = 34.1;
+            double y = 27.3;
+            double sum = x + y;
+            Console.WriteLine(sum);
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
