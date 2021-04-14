ca30fa6787b403ac9f47e3bfcb223491470a2587
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-14 11:49:37 -0300

    Testing Git Extensions also created a project to test new concepts.

---
+++ gradebook/ConceptSandBox/ChainingConstructorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptSandBox
+
+{
+    public class ChainingConstructorTest 
+    {
+        public ChainingConstructorTest(string a) : this(a, "")
+        {
+            Console.WriteLine("Constructor 1");
+        }
+        public ChainingConstructorTest(string a, string b) : this(a, b, "")
+        {
+            Console.WriteLine("Constructor 2");
+        }
+        public ChainingConstructorTest(string a, string b, string c)
+        {
+            Console.WriteLine("Constructor 3");
+        }
+
+    }
+}
---
+++ gradebook/ConceptSandBox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConceptSandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChainingConstructorTest exampleObject = new ChainingConstructorTest("a");
+        }
+    }
+}
